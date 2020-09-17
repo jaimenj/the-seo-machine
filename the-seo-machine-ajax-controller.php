6fd0d28def8bb6664144d155049e4a83262be5d4
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-09-17 20:36:20 +0200

    Starting the AJAX calls.

--- the-seo-machine-ajax-controller.php
+++ the-seo-machine-ajax-controller.php
@@ -18,6 +18,8 @@ class TheSeoMachineAjaxController
     private function __construct()
     {
         add_action('wp_ajax_tsm_urls', [$this, 'tsm_urls']);
+        add_action('wp_ajax_tsm_get_status', [$this, 'tsm_get_status']);
+        add_action('wp_ajax_tsm_do_batch', [$this, 'tsm_do_batch']);
     }
 
     public function tsm_urls()
@@ -37,5 +39,49 @@ class TheSeoMachineAjaxController
         wp_die();
     }
 
-   
+    public function tsm_get_status()
+    {
+        if (!current_user_can('administrator')) {
+            wp_die(__('Sorry, you are not allowed to manage options for this site.'));
+        }
+
+        global $wpdb;
+
+        // Check if empty..
+
+        // Check if processing..
+
+        // Check if finished..
+
+        $sql = 'SELECT ..';
+        $results = $wpdb->get_results($sql);
+
+        // Return data..
+
+        wp_die();
+    }
+
+    public function tsm_do_batch()
+    {
+        if (!current_user_can('administrator')) {
+            wp_die(__('Sorry, you are not allowed to manage options for this site.'));
+        }
+
+        global $wpdb;
+
+        $sql = 'SELECT count(*) FROM ';
+        $results = $wpdb->get_results($sql);
+
+        // If starting study..
+
+
+        // Request data to the DB..
+        
+
+        
+
+        // Return data..
+
+        wp_die();
+    }
 }
